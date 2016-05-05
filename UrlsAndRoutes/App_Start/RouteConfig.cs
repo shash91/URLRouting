9683aec7816cb3e101bf622563b494d2a411fc43
shash91 <shashika@davton.com>
2016-05-05 12:05:08 +0530

    Introducing URL patterns
    Creating and registering a simple route

---
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UrlsAndRoutes
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            Route myRoute = new Route("{controller}/{action}", new MvcRouteHandler());
+            routes.Add("MyRoute", myRoute);
+          //  routes.MapRoute("MyRoute", "{controller}/{action}");
+        }
+    }
+}
